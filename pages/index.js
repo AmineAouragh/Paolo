b37c212204925469a1701484c365c459658550d1
AMINEAOURAGH <amine.aouragh.20@gmail.com>
2026-05-14 21:02:41 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Link from "next/link"
 import Head from "next/head"
 import { useState, useEffect } from "react"
 import { Boldonse, Poppins } from 'next/font/google'
+import { initializePaddle } from "@paddle/paddle-js"
+import { supabaseClient } from "./api/supabaseClient"
+import { client } from "../pages/api/auth"
 
 const boldonse = Boldonse({
     subsets: ['latin'],
@@ -20,6 +23,29 @@ const poppinsBold = Poppins({
 })
 
 export default function Home() {
+
+  const [ paddleInstance, setPaddleInstance ] = useState(null)
+
+  let productItem = [
+    {
+      priceId: process.env.NEXT_PUBLIC_FAKE_PRICE_ID
+    }
+  ]
+
+  async function openCheckout(){
+      const paddle = await initializePaddle({
+        token: process.env.NEXT_PUBLIC_PAOLO_PADDLE_TEST_CLIENT_SIDE_TOKEN,
+        environment: "sandbox",
+        eventCallback: (data) => {
+          if (data.name == "checkout.completed"){
+            console.log(data)
+          }
+        }
+      })
+      paddle.Checkout.open({
+        items: productItem,
+      })
+  }
 
   const placeholders = [
     "A SaaS that helps creators turn long videos into short clips...",
@@ -75,7 +101,7 @@ export default function Home() {
             <div className="py-20 flex flex-col justify-center items-center w-full sm:w-3/4 md:w-2/3 xl:w-1/2">
               <h2 className={`mb-8 text-xl text-white ${boldonse.className}`}>Paolo</h2>
               <h1 className={`text-center text-white text-5xl ${boldonse.className} leading-[92px]`}>Research Your Competitors In Minutes, Not Days</h1>
-              <p className={`${poppinsBold.className} text-center text-white mt-10 text-xl`}>Just describe your app idea and Paolo takes care of the rest.</p>
+              <p className={`${poppinsBold.className} text-center text-white mt-10 text-xl`}>Just describe your business idea and Paolo takes care of the rest.</p>
               <div className="w-3/4 flex flex-col border-2 border-[#F6DC3B] shadow-lg shadow-[#F6DC3B] rounded-lg mt-10">
                 <textarea 
                   id="input_field" 
@@ -93,7 +119,7 @@ export default function Home() {
 
                   </div>
                   <button type="button" className="bg-[#F6DC3B] p-4 rounded-lg text-xl font-bold">
-                    Send
+                    <p className={`${boldonse.className}`}>Start</p>
                   </button>
                 </div>
               </div>
